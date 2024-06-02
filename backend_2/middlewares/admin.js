07c55863cc0f1d22cbc88eff066e361906a428ca
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-02 23:13:49 +0530

    admin middleware role added in token

--- backend_2/middlewares/admin.js
+++ backend_2/middlewares/admin.js
@@ -6,8 +6,10 @@ const { Admin } = require('../db')
 async function adminMiddleware(req, res, next) {
   const token = req.headers.authorization
   const jwtToken = token.split(' ')[1]
-  const decodedtoken = jwt.verify(jwtToken, jwtSecret)
-  if (decodedtoken.username) {
+  const d̥ecodedtoken = jwt.verify(jwtToken, jwtSecret)
+
+  if (d̥ecodedtoken.username && d̥ecodedtoken.role === 'admin') {
+    req.headers.username = d̥ecodedtoken.username
     next()
   } else {
     res.status(403).json({ msg: 'User Authentication Failed' })
