b4fad3dffb540052e638d66583433da4801d0db6
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-10 11:33:49 +0530

    Added the env file and added varibles in index

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,6 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const app = express()
+
+const PORT = process.env.EXPRESS_PORT || 3000
