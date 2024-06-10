edb85d78140d46eccc6c9f483660d323e193cf31
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-10 18:09:47 +0530

    Added the server port index

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 const PORT = process.env.EXPRESS_PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
