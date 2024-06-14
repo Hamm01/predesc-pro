d63481b60a9ead6fb9511a20a1af1be89339de6b
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-14 12:53:19 +0530

    Added all the schema declaration for admin user and course in

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,3 +2,19 @@ const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI)
+
+const AdminSchema = new mongoose.Schema({})
+
+const UserSchema = new mongoose.Schema({})
+
+const CourseSchema = new mongoose.Schema({})
+
+const Admin = mongoose.model('Admin', AdminSchema)
+const User = mongoose.model('Admin', UserSchema)
+const Course = mongoose.model('Admin', CourseSchema)
+
+module.exports = {
+  Admin,
+  User,
+  Course
+}
