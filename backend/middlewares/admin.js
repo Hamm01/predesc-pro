720da2c649e23c8ec3378d18e3b07f4ae15601e0
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-15 12:57:15 +0530

    adminMiddleware spefied for the auth logic

---
+++ backend/middlewares/admin.js
@@ -0,0 +1,6 @@
+function adminMiddleware(req, res, next) {
+  // Implementing the admin auth logic her
+  // taking the headers info to validate from the admin db
+}
+
+module.exports = adminMiddleware
