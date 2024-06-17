c684882c4b521924a2b527495a0e6848afe67c84
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-17 12:05:56 +0530

    Admin middleware done with headers information to check

--- backend/middlewares/admin.js
+++ backend/middlewares/admin.js
@@ -1,6 +1,20 @@
-function adminMiddleware(req, res, next) {
-  // Implementing the admin auth logic her
-  // taking the headers info to validate from the admin db
+const Admin = require('../db')
+
+async function adminMiddleware(req, res, next) {
+  const username = req.headers.username
+  const password = req.headers.password
+  try {
+    const admin = await Admin.findOne({ username, password })
+    if (admin) {
+      next()
+    } else {
+      res.status(403).json({ msg: 'User not exists' })
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send('Internal Server Error: An error occurred on the server')
+  }
 }
 
 module.exports = adminMiddleware
