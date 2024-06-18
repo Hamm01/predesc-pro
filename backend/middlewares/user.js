5260b69596a7d028e17d5dac7c38a2628ac78b33
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-18 11:08:13 +0530

    User middleware done on user side

--- backend/middlewares/user.js
+++ backend/middlewares/user.js
@@ -1,6 +1,20 @@
-function userMiddleware(req, res, next) {
-  // Implementing the user auth logic her
-  // taking the headers info to validate from the user db
+const { User } = require('../db')
+
+async function userMiddleware(req, res, next) {
+  const username = req.headers.username
+  const password = req.headers.password
+  try {
+    const UserExist = await User.findOne({ username, password })
+    if (UserExist) {
+      next()
+    } else {
+      res.status(403).json({ msg: 'User not exists' })
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send('Internal Server Error: An error occurred on the server')
+  }
 }
 
 module.exports = userMiddleware
