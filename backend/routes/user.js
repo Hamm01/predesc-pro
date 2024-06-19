f6ae755d05d9e0bda8690b5ad1e416791cf50f61
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-19 10:41:49 +0530

    user middleware introduced in user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
-
+const userMiddleware = require('../middlewares/user')
 const router = Router()
 
 router.post('/signup', (req, res) => {
   // User Signup Route
 })
-router.post('/signin', (req, res) => {
+router.post('/signin', userMiddleware, (req, res) => {
   // User Login Route
 })
 
-router.post('/courses/:courseId', (req, res) => {
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
   // Implementing the Course puchase Feature
 })
 
-router.post('/purchasedCourses', (req, res) => {
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
   // To Fetch the purchased courses
 })
 
