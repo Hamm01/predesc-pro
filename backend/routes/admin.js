fc9dde9437de100a1da016f94f9ffe819b938a81
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-19 19:11:37 +0530

    Signup route for Admin done

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
 const adminMiddleware = require('../middlewares/admin')
+const { Admin } = require('../db')
 const router = Router()
 
-router.post('/signup', (req, res) => {
-  // Signup Route
+router.post('/signup', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    const adminExist = await Admin.findOne({ username })
+    if (adminExist) {
+      res.status(409).json({ msg: 'Admin Already exists' })
+      return
+    }
+    await Admin.create({ username, password })
+    res.json({ msg: 'Admin Created Succesfully' })
+  } catch (error) {
+    res.status(500).json({ msg: 'Internal Server Error' })
+  }
 })
 
 router.post('/signin', adminMiddleware, (req, res) => {
