e2373c9d464dae5721595bacd5b18f8553fd869d
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-20 09:21:10 +0530

    changes in signup route of admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -6,17 +6,22 @@ const router = Router()
 router.post('/signup', async (req, res) => {
   const username = req.body.username
   const password = req.body.password
-
-  try {
-    const adminExist = await Admin.findOne({ username })
-    if (adminExist) {
-      res.status(409).json({ msg: 'Admin Already exists' })
-      return
+  if (username != '' && password != '') {
+    try {
+      const adminExist = await Admin.findOne({ username })
+      if (adminExist) {
+        res.status(409).json({ msg: 'Admin Already exists' })
+        return
+      }
+      await Admin.create({ username, password })
+      res.json({ msg: 'Admin Created Succesfully' })
+    } catch (error) {
+      res.status(500).json({ msg: 'Internal Server Error' })
     }
-    await Admin.create({ username, password })
-    res.json({ msg: 'Admin Created Succesfully' })
-  } catch (error) {
-    res.status(500).json({ msg: 'Internal Server Error' })
+  } else {
+    res
+      .status(400)
+      .json({ msg: 'Bad Request username and password cannot be empty' })
   }
 })
 
