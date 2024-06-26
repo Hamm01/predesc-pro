6b595e316f0e3351c8ef9cdd3da994f8d46620b8
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-26 22:46:47 +0530

    User Signup route done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,28 @@
 const { Router } = require('express')
 const userMiddleware = require('../middlewares/user')
+const { User } = require('../db')
 const router = Router()
 
-router.post('/signup', (req, res) => {
-  // User Signup Route
+router.post('/signup', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  if (username != '' && password != '') {
+    try {
+      const userExist = await User.findOne({ username })
+      if (userExist) {
+        res.status(409).json({ msg: 'User Already exists' })
+        return
+      }
+      await User.create({ username, password })
+      res.json({ msg: 'User created succesfuly' })
+    } catch (error) {
+      res.status(500).json({ msg: 'Internal Server Error' })
+    }
+  } else {
+    res
+      .status(400)
+      .json({ msg: 'Bad Request username and password cannot be empty' })
+  }
 })
 router.post('/signin', userMiddleware, (req, res) => {
   // User Login Route
