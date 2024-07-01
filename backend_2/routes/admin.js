6ecc99c7497484b498de8428779bb8f522c64fb7
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-01 22:33:41 +0530

    Added the JWT sign in route of admin

--- backend_2/routes/admin.js
+++ backend_2/routes/admin.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const adminMiddleware = require('../middlewares/admin')
 const { Admin, Course } = require('../db')
+const jwt = require('jsonwebtoken')
+const JWTSECRET = process.env.JWT_SECRET
 const router = Router()
 
 router.post('/signup', async (req, res) => {
@@ -25,7 +27,22 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-router.post('/signin', async (req, res) => {})
+router.post('/signin', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    const adminExist = await Admin.findOne({ username, password })
+    if (adminExist) {
+      const token = jwt.sign({ username }, JWTSECRET)
+      res.json({ token })
+    } else {
+      res.status(411).json({ msg: 'Invalid user details' })
+    }
+  } catch (err) {
+    res.status(500).json({ msg: 'Internal Server Error' })
+  }
+})
 
 router.post('/courses', adminMiddleware, async (req, res) => {})
 
