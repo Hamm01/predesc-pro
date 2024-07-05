94b25fe3bf1d89f1e3c4382b60ed7f6b630289f3
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-05 10:44:15 +0530

    Signup user route added

--- backend_2/routes/user.js
+++ backend_2/routes/user.js
@@ -5,7 +5,27 @@ const jwt = require('jsonwebtoken')
 
 const router = Router()
 
-router.post('/signup', async (req, res) => {})
+router.post('/signup', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  if (username != '' && password != '') {
+    try {
+      const userExist = await User.findOne({ username })
+      if (userExist) {
+        res.status(409).json({ msg: 'User Already exists' })
+        return
+      }
+      await User.create({ username, password })
+      res.json({ msg: 'User created succesfuly' })
+    } catch (error) {
+      res.status(500).json({ msg: 'Internal Server Error' })
+    }
+  } else {
+    res
+      .status(400)
+      .json({ msg: 'Bad Request username and password cannot be empty' })
+  }
+})
 router.get('/signin', async (req, res) => {})
 
 router.get('/courses', async (req, res) => {})
