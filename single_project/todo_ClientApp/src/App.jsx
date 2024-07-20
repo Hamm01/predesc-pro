22bf655ecf0dc6b2f68523f5760630364bbc6aed
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-20 08:54:14 +0530

    added the state variables and fetch request added

--- single_project/todo_ClientApp/src/App.jsx
+++ single_project/todo_ClientApp/src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import CreateTodo from './components/CreateTodo'
+import Todo from './components/Todo'
+import todoFetchRequest from './db/todo'
 function App() {
+  const [todos, setTodos] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await todoFetchRequest()
+
+      setTodos(response)
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
       <CreateTodo></CreateTodo>
+      <Todo todos={todos}></Todo>
     </>
   )
 }
